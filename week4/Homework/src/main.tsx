8de11ae39fb5f799d72b4b1e3924e041949c047f
이소희 <sohee@isohuiui-MacBookAir.local>
2026-05-06 18:37:30 +0900

    feat:회원가입페이지 구조 구현

--- week4/Homework/src/main.tsx
+++ week4/Homework/src/main.tsx
@@ -4,10 +4,11 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-);
+);    
--- week4/Homework/src/pages/SignupPage.tsx
+++ week4/Homework/src/pages/SignupPage.tsx
@@ -1,5 +1,73 @@
+import { Link } from "react-router-dom";
+
 function SignupPage() {
-  return <div>SignupPage</div>;
+  return (
+    <main>
+      <h1>회원가입</h1>
+
+      <form>
+        <div>
+          <label htmlFor="signupId">아이디</label>
+          <input
+            type="text"
+            id="signupId"
+            name="signupId"
+            placeholder="아이디를 입력하세요"
+          />
+        </div>
+        <div>
+          <label htmlFor="password">비밀번호</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="비밀번호를 입력하세요"
+          />
+        </div>
+        <div>
+          <label htmlFor="name">이름</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="이름을 입력하세요"
+          />
+        </div>
+        <div>
+          <label htmlFor="email">이메일</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="이메일을 입력하세요"
+          />
+        </div>
+        <div>
+          <label htmlFor="age">나이</label>
+          <input
+            type="number"
+            id="age"
+            name="age"
+            placeholder="나이를 입력하세요"
+          />
+        </div>
+        <div>
+          <label htmlFor="part">파트</label>
+          <input
+            type="text"
+            id="part"
+            name="part"
+            placeholder="파트를 입력하세요"
+          />
+        </div>
+
+        <button type="submit">회원가입</button>
+      </form>
+      <p>
+        이미 계정이 있나요? <Link to="/login">로그인</Link>
+      </p>
+    </main>
+  );
 }
 
 export default SignupPage;
